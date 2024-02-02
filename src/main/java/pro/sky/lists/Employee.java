33123bfa1b101b1a6fc23a10f53515fe8b702fc7
AliceVoice <poctuk22@gmail.com>
2024-02-02 16:36:15 +0300

    homework done

--- src/main/java/pro/sky/lists/Employee.java
+++ src/main/java/pro/sky/lists/Employee.java
@@ -5,16 +5,10 @@ import java.util.Objects;
 public class Employee {
     private String firstName;
     private String lastName;
-    private String fullname = firstName + " " + lastName;
-
 
     public Employee(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-    }
-
-    public String getFullname() {
-        return fullname;
     }
 
     public String getFirstName() {
@@ -27,7 +21,7 @@ public class Employee {
 
     @Override
     public String toString() {
-        return "ФИО сотрудника: " + getFullname();
+        return "ФИО сотрудника: " + getFirstName() + " " + getLastName();
     }
 
     @Override
@@ -35,11 +29,11 @@ public class Employee {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
-        return Objects.equals(fullname, employee.fullname);
+        return Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(fullname);
+        return Objects.hash(firstName, lastName);
     }
 }
